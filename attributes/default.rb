b4acce55c7b3ed709f728432e8b74a907c0496bb
Yen-Nan Lin <yennanlin.tw@gmail.com>
2018-07-19 14:13:53 +0800

    Add docker cookbook and use bitnami/memcached

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['memcached']['sasl_user_name'] = 'admin'
+default['memcached']['sasl_user_password'] = 'admin_password'
+default['memcached']['memory'] = 128
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,21 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+docker_service 'default' do
+  action [:create, :start]
+end
+
+docker_image 'bitnami/memcached:1.5.9-debian-9' do
+  action :pull_if_missing
+end
+
+docker_container 'memcached_server' do
+  action :run_if_missing
+  repo 'bitnami/memcached:1.5.9-debian-9'
+  port "11211:11211"
+  env ["MEMCACHED_CACHE_SIZE=#{node['memcached']['memory']}",
+       "MEMCACHED_USERNAME=#{node['memcached']['sasl_user_name']}",
+       "MEMCACHED_PASSWORD=#{node['memcached']['sasl_user_password']}"]
+  restart_policy 'always'
+end
